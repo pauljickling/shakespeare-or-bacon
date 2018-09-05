8d035c3f6f9810e23705e3ff277b788a8a81a3db
Paul Jickling <pjickling@gmail.com>
2018-09-04 23:28:39 -0700

    Implemented function to remove digits from text

--- clean_text.py
+++ clean_text.py
@@ -1,4 +1,14 @@
+def clean(text):
+	new = text.lstrip().strip('0123456789')
+	return new
+
+shakespeare_set = set()
+
 TEXTFILE = "src_text/shakespeare.txt"
-text = open(TEXTFILE, 'r')
-shakespeare = text.read()
-print(shakespeare)
+with open(TEXTFILE) as file:
+	for i, line in enumerate(file):
+		if i > 300 and i < 400:
+			new_line = clean(line)
+			shakespeare_set.add(new_line.rstrip())
+
+print(shakespeare_set)
