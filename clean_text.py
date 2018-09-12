67766e02c71f81eae073a6aa4fe09275ef5201ec
Paul Jickling <pjickling@gmail.com>
2018-09-12 15:44:30 -0700

    Reimplemented clean_text.py which was deleted by mistake

---
+++ clean_text.py
@@ -0,0 +1,30 @@
+# parses a string into separates words, and appends them to a returned list
+def parse_words(str):
+	str = str + ' '
+	word = ''
+	word_list = []
+	for i in str:
+		if i == ' ':
+			word_list.append(word)
+			word = ''
+		else:
+			word = word + i
+	return word_list
+
+word_list = []
+
+TEXTFILE = "src_text/shakespeare.txt"
+with open(TEXTFILE) as file:
+	for i, line in enumerate(file):
+		if i > 300 and i < 400:
+			new_line = line.lstrip().strip('0123456789')
+			new_line = new_line.rstrip()
+			new_line = new_line.replace('.', ' ')
+			new_line = new_line.replace(',', ' ')
+			new_line = new_line.replace('?', ' ')
+			new_line = new_line.replace('!', ' ')
+			new_line = new_line.replace(';', ' ')
+			new_line = new_line.replace(':', ' ')
+			parsed = parse_words(new_line)
+			for word in parsed:
+				word_list.append(word)
