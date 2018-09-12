a36d24d216a82fda9316d23ea3d920cdb129339b
Paul Jickling <pjickling@gmail.com>
2018-09-12 15:58:35 -0700

    Removed character names

--- clean_text.py
+++ clean_text.py
@@ -1,3 +1,5 @@
+import re
+
 # parses a string into separates words, and appends them to a returned list
 def parse_words(str):
 	str = str + ' '
@@ -16,7 +18,7 @@ word_list = []
 TEXTFILE = "src_text/shakespeare.txt"
 with open(TEXTFILE) as file:
 	for i, line in enumerate(file):
-		if i > 300 and i < 400:
+		if i > 3000 and i < 3400:
 			new_line = line.lstrip().strip('0123456789')
 			new_line = new_line.rstrip()
 			new_line = new_line.replace('.', ' ')
@@ -28,3 +30,11 @@ with open(TEXTFILE) as file:
 			parsed = parse_words(new_line)
 			for word in parsed:
 				word_list.append(word)
+
+print("First word list: ", word_list)
+
+for i in word_list:
+	if i.isupper():
+		word_list.remove(i)
+
+print("Second word list: ", word_list)
