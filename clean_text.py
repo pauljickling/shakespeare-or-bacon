d703cb9d19a8eecdc0927406695f7c4e8d38e232
Paul Jickling <pjickling@gmail.com>
2018-09-12 23:57:53 -0700

    Improved word list file

--- clean_text.py
+++ clean_text.py
@@ -16,25 +16,23 @@ word_list = []
 TEXTFILE = "filter_text/shakespeare.txt"
 with open(TEXTFILE) as file:
 	for i, line in enumerate(file):
-		if i > 3000 and i < 3400:
-			new_line = line.lstrip().strip('0123456789')
-			new_line = new_line.rstrip()
-			new_line = new_line.replace('.', ' ')
-			new_line = new_line.replace(',', ' ')
-			new_line = new_line.replace('?', ' ')
-			new_line = new_line.replace('!', ' ')
-			new_line = new_line.replace(';', ' ')
-			new_line = new_line.replace(':', ' ')
-			parsed = parse_words(new_line)
-			for word in parsed:
-				word_list.append(word)
-
-for i in word_list:
-	if i.isupper():
-		word_list.remove(i)
-	if i == '':
-		word_list.remove(i)
+		new_line = line.lstrip().strip('0123456789')
+		new_line = new_line.rstrip()
+		new_line = new_line.replace('.', ' ')
+		new_line = new_line.replace(',', ' ')
+		new_line = new_line.replace('?', ' ')
+		new_line = new_line.replace('!', ' ')
+		new_line = new_line.replace(';', ' ')
+		new_line = new_line.replace(':', ' ')
+		new_line = new_line.replace('"', ' ')
+		parsed = parse_words(new_line)
+		for word in parsed:
+			if word.isupper() == False:
+				if word != '':
+					word_list.append(word)
 
 with open('filter_text/shakespeare_word_list.txt', 'w') as file:
+	file.write('[')
 	for i in word_list:
-		file.write(i)
+		file.write(i.lower() + ", ")
+	file.write(']')
