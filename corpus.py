4b4c3a59996e0a56b7b0dd02a645fec394cbeae7
Paul Jickling <pjickling@gmail.com>
2018-10-01 15:32:11 -0700

    Hapax calculations introduced

--- corpus.py
+++ corpus.py
@@ -16,3 +16,30 @@ fdist5 = FreqDist(text5)
 
 shakespeare_list = fdist5.hapaxes()
 bacon_list = fdist1.hapaxes()
+shake_numless = []
+bacon_numless = []
+
+for i in bacon_list:
+	if not i.isdigit():
+		bacon_numless.append(i)
+
+for i in shakespeare_list:
+	if not i.isdigit():
+		shake_numless.append(i)
+
+shake_len = len(shake_numless)
+bacon_len = len(bacon_numless)
+total_len = shake_len + bacon_len
+
+counter = 0
+shared_list = []
+
+for i in bacon_numless:
+	if i in shake_numless:
+		counter += 1
+		shared_list.append(i)
+
+print("Shared hapaxes: ", counter)
+print("Shakespeare hapax list length: ", shake_len)
+print("Bacon hapax list length: ", bacon_len)
+print("Total hapax list length: ", total_len)
