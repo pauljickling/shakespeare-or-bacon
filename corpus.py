9bbc0cca005b5b1ecb515a2eec5f7d048d136a0d
Paul Jickling <pjickling@gmail.com>
2018-10-01 15:19:28 -0700

    Initial test for hapaxes

--- corpus.py
+++ corpus.py
@@ -1,8 +1,18 @@
 from nltk import FreqDist, Text
 from nltk.corpus import PlaintextCorpusReader
+
 corpus_root = 'filter_text'
 books = PlaintextCorpusReader(corpus_root, '.*')
-print(len(books.fileids()))
+#print(books.fileids())
 text1 = books.words(fileids=['essays_and_wisdom_of_the_ancients.txt'])
+text2 = books.words(fileids=['new_atlantis.txt'])
+text3 = books.words(fileids=['novum_organum.txt'])
+text4 = books.words(fileids=['of_gardens.txt'])
+text5 = books.words(fileids=['shakespeare.txt'])
+text6 = books.words(fileids=['the_advancement_of_learning.txt'])
+
 fdist1 = FreqDist(text1)
-print(fdist1)
+fdist5 = FreqDist(text5)
+
+shakespeare_list = fdist5.hapaxes()
+bacon_list = fdist1.hapaxes()
